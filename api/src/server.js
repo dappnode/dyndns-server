6facbe88e4246a6f1e88c97b8c472fcc767dbe63
vdo <vdo@greyfaze.net>
2018-09-26 15:41:23 +0200

    JSON responses

--- api/src/server.js
+++ api/src/server.js
@@ -1,22 +1,28 @@
 const express = require('express');
 const rateLimit = require('express-rate-limit');
 const EthCrypto = require('eth-crypto');
+const nsupdate = require('./util/nsupdate');
 
 // Load configuration
 const config = require('./config');
-const nsupdate = require('./util/nsupdate');
 
 var app = express();
 
-app.enable("trust proxy"); // Trust our NGINX
+// Trust NGINX proxy
+app.enable("trust proxy"); 
  
+// Apply the rate limit to all requests
 const limiter = rateLimit({
-  windowMs: parseInt(config.limit_window) * 60 * 1000, // 
-  max: parseInt(config.limit_rate) // limit each IP to N requests 
+  windowMs: parseInt(config.limit_window) * 60 * 1000,
+  max: parseInt(config.limit_rate)
 });
- 
-// Apply the limit to all requests
 app.use(limiter);
+
+// Middleware to return JSON header
+app.use(function (req, res, next) {
+    res.header('Content-Type', 'application/json');
+    next();
+});
 
 app.get('/', async function (req, res) {
     var address = req.query.address;
@@ -29,19 +35,19 @@ app.get('/', async function (req, res) {
     if (threshold >= timestamp ) console.log(`Warning: Threshold ${threshold} is bigger than timestamp.`);
 
     if (!timestamp && !sig && !address ) {
-        res.status(200).send(config.message);
+        res.status(200).send(JSON.stringify({ message: config.message }));
         return
     }
 
     if (!timestamp || !sig || !address ) {
-        res.status(400).send("Missing parameter(s)");
+        res.status(400).send(JSON.stringify({ message: "Missing parameter(s)"}));
         return
     }
 
     try {
         signAddress = EthCrypto.recover(sig,EthCrypto.hash.keccak256(timestamp.toString()));
     } catch (err){
-        res.status(400).send(`${err}`);
+        res.status(400).send(JSON.stringify({ message: "Signing error: "+err.message}));
         return
     }
 
@@ -49,22 +55,27 @@ app.get('/', async function (req, res) {
     const validTimestamp = ( epoch <= timestamp + threshold ) && ( epoch >= timestamp - threshold );
  
     if ( signAddress.toLowerCase() !== address.toLowerCase() ) {
-        res.status(400).send("Invalid address or signature.");
+        res.status(400).send(JSON.stringify({ message: "Invalid address or signature."}));
     } else if (!validTimestamp) {
-        res.status(400).send("Timestamp out of sync. Is your server syncronized?");
+        res.status(400).send(JSON.stringify({ message: "Timestamp out of sync. Is your server syncronized?"}));
     } else {
         // Grab only first 8 chars of address as subdomain
         var subdomain = address.toLowerCase().substr(2).substring(0,8);
+        // Grab ipv4 only
         var remoteIP = req.ip.includes(':') ? req.ip.split(':')[3] : req.ip
         try {
             var result = await nsupdate(config.bind_server, config.zone, subdomain, config.ttl, remoteIP);
             if (result) {
-                res.status(200).send(`Your dynamic domain ${subdomain}.${config.zone} has been updated to ${remoteIP}`);
+                res.status(200).send(JSON.stringify({
+                    ip: remoteIP,
+                    domain: `${subdomain}.${config.zone}`,
+                    message: `Your dynamic domain ${subdomain}.${config.zone} has been updated to ${remoteIP}`})
+                );
             }
         } catch (err){
-            res.status(500).send(`Error on update: ${err}`);
+            res.status(500).send(JSON.stringify({ message: "Internal error on update."}));
+            console.log(err.message);
         }
-        
     }
 });
 
