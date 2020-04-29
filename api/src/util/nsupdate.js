2fc611924b05be7bc585597a3dbf7944bf537771
vdo <vdo@greyfaze.net>
2020-04-29 12:57:39 +0200

    Add wildcard for SSL certs, prevent zone overwrite

--- api/src/util/nsupdate.js
+++ api/src/util/nsupdate.js
@@ -1,20 +1,22 @@
-const shell = require('shelljs');
+const shell = require("shelljs");
 
-async function nsupdate(server, zone, address, ttl,  ip) {
-    //var dnsType = 'A'
-    shell.config.silent = true
-    const cmdTimeout = 10 * 1000;
-    const nsupdateCmd = 'nsupdate -v'
-    var updateCmd = `server ${server}\ndebug yes\nzone ${zone}\nupdate delete ${address}.${zone} A\nupdate add ${address}.${zone} ${ttl} A ${ip}\nshow\nsend`
-    const res = await shell.echo(updateCmd).exec(nsupdateCmd,{timeout: cmdTimeout});
+async function nsupdate(server, zone, address, ttl, ip) {
+  //var dnsType = 'A'
+  shell.config.silent = true;
+  const cmdTimeout = 10 * 1000;
+  const nsupdateCmd = "nsupdate -v";
+  var updateCmd = `server ${server}\ndebug yes\nzone ${zone}\nupdate delete ${address}.${zone} A\nupdate add ${address}.${zone} ${ttl} A ${ip}\nupdate add *.${address}.${zone} ${ttl} A ${ip}\nshow\nsend`;
+  const res = await shell
+    .echo(updateCmd)
+    .exec(nsupdateCmd, { timeout: cmdTimeout });
 
-    if (!res) throw Error(command+' expired timeout: ('+maxTime+' ms)');
+  if (!res) throw Error(command + " expired timeout: (" + maxTime + " ms)");
 
-    if (res.code !== 0) {
-        const err = res.stderr.length ? res.stderr : res.stdout;
-        throw Error(err);
-      }
-    return res.stdout;    
+  if (res.code !== 0) {
+    const err = res.stderr.length ? res.stderr : res.stdout;
+    throw Error(err);
+  }
+  return res.stdout;
 }
 
 module.exports = nsupdate;
--- bind/init.sh
+++ bind/init.sh
@@ -5,7 +5,10 @@ export UPDATE_HOST=${UPDATE_HOST:-127.0.0.1}
 export RECORD_TTL=${RECORD_TTL:-30}
 export DLR='$'
 
-envsubst < "/etc/bind/templates/template.zone" > "/etc/bind/$ZONE.zone"
-envsubst < "/etc/bind/templates/named.conf" > "/etc/bind/named.conf"
+// Do not overwrite existing zone file
+if [ ! -f "/etc/bind/$ZONE.zone" ]; then
+    envsubst <"/etc/bind/templates/template.zone" >"/etc/bind/$ZONE.zone"
+fi
+envsubst <"/etc/bind/templates/named.conf" >"/etc/bind/named.conf"
 
 /usr/sbin/named -d 1 -f -c /etc/bind/named.conf
