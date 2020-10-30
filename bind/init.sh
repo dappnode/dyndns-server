fa793301de759e055b16a772f7575327229634be
Luka Gulin <lgulin@phy.hr>
2020-10-30 15:37:58 +0100

    Tweaked default keypatsh

--- bind/init.sh
+++ bind/init.sh
@@ -5,7 +5,7 @@ export UPDATE_HOST=${UPDATE_HOST:-127.0.0.1}
 export RECORD_TTL=${RECORD_TTL:-30}
 export DLR='$'
 
-export KEY_PATH=${KEY_PATH:-"/etc/bind/.secrets/keyfile.key"}
+export KEY_PATH=${KEY_PATH:-"/etc/certbot/keyfile.key"}
 export KEY_NAME=${KEY_NAME:-"keyname"}
 
 # Do not overwrite existing zone file
