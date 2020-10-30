336721d76bf8ce823d0ae7b0feae3c39829a9506
Luka Gulin <lgulin@phy.hr>
2020-10-30 15:35:52 +0100

    Completed config changes

--- bind/init.sh
+++ bind/init.sh
@@ -5,8 +5,8 @@ export UPDATE_HOST=${UPDATE_HOST:-127.0.0.1}
 export RECORD_TTL=${RECORD_TTL:-30}
 export DLR='$'
 
-export KEY_PATH=${KEY_PATH:-"/etc/bind/.secrets/certbot.key"}
-export KEY_NAME=${KEY_NAME:-"certbot"}
+export KEY_PATH=${KEY_PATH:-"/etc/bind/.secrets/keyfile.key"}
+export KEY_NAME=${KEY_NAME:-"keyname"}
 
 # Do not overwrite existing zone file
 if [ ! -f "/etc/bind/$ZONE.zone" ]; then
